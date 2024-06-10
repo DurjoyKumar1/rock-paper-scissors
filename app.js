6a2877fd50fc7290391341f435926d9d6bfe676f
DurjoyKumar <durjoy.uk6@gmail.com>
2024-06-11 02:02:30 +0600

    final

--- app.js
+++ app.js
@@ -1,74 +1,128 @@
 let rock = document.querySelector(".rock");
 let paper = document.querySelector(".paper");
 let scissors = document.querySelector(".scissors");
-
 let allElem = document.querySelectorAll(".submain div");
+let winnerDisplay=document.querySelector(".scoreWindow");
+let displayBoard=document.querySelector(".display");
+let newRoundBtn=document.querySelector(".new-round");
+let newGamebtn = document.querySelector(".new-game");
+let playBtn = document.querySelector(".play");
 
-
+let computer = 0;
+let player = 0;
 
 function playerValue(){
     let value="";
+    displayBoard.style.display="none";
+    winnerDisplay.style.display="none";
     allElem.forEach(div=>{
         div.addEventListener("click",()=>{
-            if(div.classList.contains("rock")){
-                value = "rock";
-                console.log(value);
-                
-            }
-            else if(div.classList.contains("paper")){
-                value = "paper";
-                console.log(value);
-            }
-            else if(div.classList.contains("scissors")){
-                value = "scissors";
-                console.log(value);
-            
-            }
-            
-            winnerSelection(value,computerValue());
-            
-        });
-    
-    });
-    
-}
 
+                if(div.classList.contains("rock")){
+                    value = "rock";    
+                }
+                else if(div.classList.contains("paper")){
+                    value = "paper";                
+                }
+                else if(div.classList.contains("scissors")){
+                    value = "scissors";
+                }
+                
+                winnerSelection(value,computerValue());
+        });       
+    });
+      
+}
 
 function computerValue(){
     let value2="";
     let list=["rock","paper","scissors"];
     let index = Math.floor(Math.random()*3);
     value2=list[index];
-    return value2;
-
-    
+    return value2;  
 }
-
 
 function winnerSelection(playerValue,computerValue){
-    
+
     if(playerValue===computerValue){
-        console.log("draw");
+        winnerDisplay.innerText = "Draw This Round";
     }
     else if(computerValue==="paper" && playerValue=="rock" ){
-        console.log("Computer win");
+        computer+=1;
+        winnerDisplay.innerText = "Computer Win This Round"
+        displayBoard.style.display="block";
+        winnerDisplay.style.display="block";
     }
     else if(computerValue==="paper" && playerValue=="scissors"){
-        console.log("player win");
+        player+=1;
+        winnerDisplay.innerText = "You Win This Round"
+        displayBoard.style.display="block";
+        winnerDisplay.style.display="block";
     }
     else if(computerValue==="rock" && playerValue=="scissors" ){
-        console.log("Computer win");
+        computer+=1;
+        winnerDisplay.innerText = "Computer Win This Round"
+        displayBoard.style.display="block";
+        winnerDisplay.style.display="block";
     }
     else if(computerValue==="rock" && playerValue=="paper"){
-        console.log("player win");
+        player+=1;
+        winnerDisplay.innerText = "You Win This Round"
+        displayBoard.style.display="block";
+        winnerDisplay.style.display="block";
     }
     else if(computerValue==="scissors" && playerValue=="paper" ){
-        console.log("Computer win");
+        computer+=1;
+        winnerDisplay.innerText = "Computer Win This Round"
+        displayBoard.style.display="block";
+        winnerDisplay.style.display="block";
     }
     else if(computerValue==="scissors" && playerValue=="rock"){
-        console.log("player win");
+        player+=1;
+        winnerDisplay.innerText = "You Win This Round"
+        displayBoard.style.display="block";
+        winnerDisplay.style.display="block";
     }
-
+    scoreBoard(player, computer);
 }
-playerValue();
+
+function scoreBoard(player, computer){
+    let playerScore = document.querySelector(".player");
+    let computerScore = document.querySelector(".computer");
+    playerScore.innerText ="Player: " + player;
+    computerScore.innerText = "Computer: " + computer;
+}
+
+function newRound(){
+    newRoundBtn.addEventListener("click",()=>{
+        displayBoard.style.display="none";
+        winnerDisplay.style.display="none";
+    });   
+}
+
+function newGame(){
+    newGamebtn.addEventListener("click",()=>{
+    displayBoard.style.display="none";
+    winnerDisplay.style.display="none";
+    scoreBoard(0,0);
+    
+
+    });
+}
+
+function startGame(){
+    displayBoard.style.display="none";
+    winnerDisplay.style.display="none";
+    playBtn.addEventListener("click",()=>{
+        playerValue();
+        newRound();
+        newGame();
+        scoreBoard(0,0);
+
+    });
+}
+
+startGame();
+
+
 
